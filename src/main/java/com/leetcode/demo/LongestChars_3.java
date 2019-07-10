ea5bc4664ce1b0320b27f5b924b7420a6ef71454
ssjsyswa@163.com <ssjsyswa@163.com>
2019-07-10 12:19:29 +0800

    algorithm test

---
+++ src/main/java/com/leetcode/demo/LongestChars_3.java
@@ -0,0 +1,27 @@
+package com.leetcode.demo;
+
+/**
+ * @Author Jsy
+ * @Date ${Date}
+ * @ /**
+ * 找到字符串中不重复的最长长度.
+ * @date: 2019-07-10
+ * @version: 1.0
+ * @author: jiangshuyi@beyondsoft.com
+ */
+public class LongestChars_3 {
+    public static void main(String[] args) {
+        // String
+        String a = "abc";
+        String b = "aabcd";
+        String c = "aabcc";
+        
+        int length = findLongestChar(a);
+        
+    }
+
+    private static int findLongestChar(String a) {
+
+        return -1 ;
+    }
+}
